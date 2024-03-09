e181d6bb43dc7a0d7a183fb583ea2ed893a3bdf9
Kevin NZUGUEM <nzuguemkevine@gmail.com>
2024-03-09 08:55:32 +0100

    ⭐Setup MUSL

---
+++ scripts/setup-musl.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+
+ZLIB_VERSION=1.2.13
+TOOLCHAIN_DIR=`pwd`/x86_64-linux-musl-native
+CC=${TOOLCHAIN_DIR}/bin/gcc
+PATH=${TOOLCHAIN_DIR}/bin:${PATH}
+
+# Download musl
+wget -q http://more.musl.cc/10/x86_64-linux-musl/x86_64-linux-musl-native.tgz
+tar -xzf x86_64-linux-musl-native.tgz
+rm x86_64-linux-musl-native.tgz
+
+# Download, build, install zlib into TOOLCHAIN_DIR
+echo "zlib version=${ZLIB_VERSION}"
+wget -q https://zlib.net/fossils/zlib-${ZLIB_VERSION}.tar.gz
+tar -xzf zlib-${ZLIB_VERSION}.tar.gz
+rm zlib-${ZLIB_VERSION}.tar.gz
+cd zlib-${ZLIB_VERSION}
+./configure --prefix=${TOOLCHAIN_DIR} --static
+make
+make install
+
+cd ..
