199790371b1a25995687d1be7e6d62a23b90ac53
Kevin NZUGUEM <nzuguemkevine@gmail.com>
2025-04-24 05:33:03 +0000

    🏷️ Bump Gradle version

--- 03-springboot/build.gradle.kts
+++ 03-springboot/build.gradle.kts
@@ -16,8 +16,8 @@ group = "me.nzuguem"
 version = "1.0"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_23
-	targetCompatibility = JavaVersion.VERSION_23
+	sourceCompatibility = JavaVersion.VERSION_24
+	targetCompatibility = JavaVersion.VERSION_24
 }
 
 repositories {
